655e38ec2fde790ff688d3189330e9450b01faa3
Brad Campbell <b@bradleyscampbell.net>
2017-08-13 20:20:00 -0400

    solution: exercise 7

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,13 @@
-def word_count():
-    pass
+def word_count(phrase):
+    filtered_phrase = ''.join([c if c.isalnum() else ' ' for c in phrase])
+    filtered_phrase_as_list = filtered_phrase.lower().split()
+    filtered_phrase_as_list.sort()
+    keys = list(set(filtered_phrase_as_list))
+    count = {key: 0 for key in keys}
+
+    for k in keys:
+        for word in filtered_phrase_as_list:
+            if word == k:
+                count[k] = count[k] + 1
+
+    return count
